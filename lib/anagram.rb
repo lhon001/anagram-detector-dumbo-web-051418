b3f461c74991906b0a1c161575971061eeafb5f3
github username <github email address>
2018-05-20 16:14:19 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,14 +5,15 @@ class Anagram
   end
 
   def match(words_array)
-    anagram_word = []
-    sorted_word = @word.split("").sort
-
-    words_array.each do |word|
-      if sorted_word == word.split("").sort
-        anagram_word << word
-      end
-    end
-    anagram_word.uniq
+    words_array.select {|array_word| array_word.split("").sort == @word.split("").sort}
+    # anagram_word = []
+    # sorted_word = @word.split("").sort
+    #
+    # words_array.each do |word|
+    #   if sorted_word == word.split("").sort
+    #     anagram_word << word
+    #   end
+    # end
+    # anagram_word.uniq
   end
 end
